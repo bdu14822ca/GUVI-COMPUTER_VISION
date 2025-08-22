ed773e01c47942f1b918ffbb40edff4969d150a0
bdu14822ca <144525900+bdu14822ca@users.noreply.github.com>
2025-08-22 11:26:05 +0530

    Add files via upload

---
+++ Day-1/cv.py
@@ -0,0 +1,11 @@
+import cv2
+
+import matplotlib.pyplot as plt
+img =cv2.imread("madhara.jpg")
+print(img.shape)
+
+
+cv2.rectangle(img,(350,400),(100,100),(255,0,0),2)
+
+plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+plt.show()
